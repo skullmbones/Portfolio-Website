c42776c721c164482b2863c9999d0474be3efea1
Michael Babboni <michaelbabboni@icloud.com>
2026-06-02 16:36:34 -0400

    react installed and very simple text displayed

---
+++ src/components/Desktop.tsx
@@ -0,0 +1,13 @@
+function Desktop() {
+  return (
+    <div className="desktop">
+
+      <main className="desktop-area">
+        <h1>Welcome to Michael OS</h1>
+      </main>
+
+    </div>
+  );
+}
+
+export default Desktop;
---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div className="login">
+
+      <main className="login-area">
+        <h1>Michael Babboni</h1>
+        <button>Login</button>
+      </main>
+
+    </div>
+  );
+}
+
+export default Login;
