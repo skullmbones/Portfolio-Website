264e2c2e58fa02b86b69a2a79b20a3850a055190
Michael Babboni <michaelbabboni@icloud.com>
2026-08-07 14:24:02 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio-Website/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/Portfolio-Website/',
 });
